3269de08f643363ea013a6c7070270697b3ffb85
Nikunj-kumar <46553945+Nikunj-kumar@users.noreply.github.com>
2019-07-04 11:40:03 +0530

    Fizzbuzz
    Check Fizzbuzz logic

---
+++ Fizzbuzz.py
@@ -0,0 +1,17 @@
+def fizzbuzz(n):
+    if n%3==0 and n%5==0:
+        print("Fizzbuzz")
+    elif n%5==0:
+        print("buzz")
+    elif n%3==0:
+        print("Fizz")
+    else:
+        print("Enter valid number")
+
+fizzbuzz(8)
+
+
+#execute it here fizzbuzz(21)
+#or go to the line then enter fizzbuzz(enter the number)
+
+
